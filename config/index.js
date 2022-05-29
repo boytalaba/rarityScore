ac1226037642699b58f0a85f4a284dfa34a82725
boytalaba <36549438+boytalaba@users.noreply.github.com>
2022-05-29 20:36:43 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "odp-rarity.vercel.app",
+  API_URL: "https://rarity-score-six.vercel.app/",
   COLLECTION_NAME: "ChickElon",
   COLLECTION_TITLE: "ChickElon NFT",
   COLLECTION_DESCRIPTION: '2222 unique generated NFTS.',
