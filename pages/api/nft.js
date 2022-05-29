28c511f4a1833476e0582c757298e8de82d47e72
boytalaba <36549438+boytalaba@users.noreply.github.com>
2022-05-29 21:51:55 +0800

    Update nft.js

--- pages/api/nft.js
+++ pages/api/nft.js
@@ -2,6 +2,6 @@ import { getNFT } from "../../util/nfts"
 
 export default function nftAPI(req, res) {
   const { id } = req.query
-  const id = getNFT(id);
+  const nft = getNFT(id);
   res.status(200).json(nft)
 }
